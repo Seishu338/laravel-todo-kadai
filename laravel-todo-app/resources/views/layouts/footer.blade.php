933565bc701d3c4eb85a665b7c761e57cf330f07
seishu338 <seikuma338@icloud.com>
2023-12-26 16:48:33 +0900

    課題の追加

---
+++ laravel-todo-app/resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div  class="d-flex justify-content-center alingn-items-center bg-light fixed-bottom" style="height: 60px; z-index: auto;">
+        <p>&copy; {{ config('appname')}} All rights reserved.</p>
+    </div>
+</footer>
